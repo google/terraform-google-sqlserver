fabdf35bb3b7d1321399424712bb57f75f665720
Luqi Tian <luqit@google.com>
2025-03-07 11:45:04 -0800

    Add no scratch disks when machine type is not n2 or m3
    PiperOrigin-RevId: 734635902

--- terraform/modules/localssd_for_machinetype/main.tf
+++ terraform/modules/localssd_for_machinetype/main.tf
@@ -18,19 +18,19 @@ variable "machine_type" {
 
 locals {
   machine_family = lower(split("-", var.machine_type)[0])
-  cpu_count = parseint(split("-", var.machine_type)[2], 10)
+  cpu_count      = parseint(split("-", var.machine_type)[2], 10)
 
   # The number of scratch disks is based on the machine type.
   # https://cloud.google.com/compute/docs/disks/local-ssd#lssd_disk_options
   m3_scratch_disk_count = local.cpu_count <= 64 ? 4 : 8
   n2_scratch_disk_count = local.cpu_count < 10 ? 1 : (
-      local.cpu_count < 20 ? 2 : (
-        local.cpu_count < 40 ? 4 : (
-          local.cpu_count < 80 ? 8 : 16
-        )
+    local.cpu_count < 20 ? 2 : (
+      local.cpu_count < 40 ? 4 : (
+        local.cpu_count < 80 ? 8 : 16
       )
+    )
   )
-  scratch_disk_count = local.machine_family == "m3" ? local.m3_scratch_disk_count : local.n2_scratch_disk_count
+  scratch_disk_count = local.machine_family == "m3" ? local.m3_scratch_disk_count : (local.machine_family == "n2" ? local.n2_scratch_disk_count : 0)
 }
 
 output "count" {
